86a966d2955c82fb3201f78a08acabd7c8d94326
senaarth <senaarth@gmail.com>
2025-05-27 14:47:07 -0300

    feat: back btn waitlist

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -1,9 +1,13 @@
 import { Waitlist } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 export default function WaitlistPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col gap-5 justify-center items-center h-screen">
       <Waitlist />
+      <Link className="text-sm hover:brightness-75" to="/">
+        Voltar
+      </Link>
     </div>
   );
 }
