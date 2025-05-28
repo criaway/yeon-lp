69f4aa32b3f7c4d4e35278dd645048eafd3abccd
lucas <lafdesouza2002@outlook.com>
2025-05-28 16:19:45 -0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ptBR } from "@clerk/localizations";
+import { inject } from '@vercel/analytics';
+inject();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
